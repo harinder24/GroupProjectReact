ba0bc2609e77754c053d4a3db15e9cabdb8b66cb
jean222 <105907450+jean222@users.noreply.github.com>
2023-07-11 15:00:39 -0700

    searchbar started
    created the search bar and added searchbar icon from mui. Still needs some changing and editing

--- instagram/app/explore/page.tsx
+++ instagram/app/explore/page.tsx
@@ -1,11 +1,26 @@
+'use client'
 import Nav from "../nav/page";
-
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 const explore = () => {
     return(
-        <>
+        <div className="flex">
         <Nav/>
-        </>
+
+        {/* Search Bar */}
+        <div className="flex flex-row h-8 items-center m-5 bg-gray-200 box-border rounded-[8px] px-2">
+            <SearchOutlinedIcon className=" text-gray-500" />
+            
+            <input type="text"
+                className=" bg-inherit text-gray-500 placeholder:text-gray-500 "
+                placeholder="Search"
+    
+                />
+            
+        </div>
+        
+        
+        </div>
     )
 }
 
