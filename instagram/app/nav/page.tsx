64f2709d8110ad357e6ff998295e985d0b2af8b6
Betty8467 <105934051+Betty8467@users.noreply.github.com>
2023-08-01 15:22:00 -0700

    fixing scroll

--- instagram/app/nav/page.tsx
+++ instagram/app/nav/page.tsx
@@ -11,11 +11,12 @@ import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 
 import "./page.css";
+import Link from "next/link";
 type Props = {};
 
 const Nav = (props: Props) => {
   return (
-    <nav className="px-3 pt-2 pb-5 bg-white flex flex-col box-border gap-y-2 w-[244px] h-screen border-r-[1px] border-solid border-[var(--border)]  z-[5] max-[1264px]:w-[72.8px] max-[770px]:fixed max-[770px]:w-full max-[770px]:flex-row max-[770px]:bottom-0 max-[770px]:h-fit max-[770px]:p-0 max-[770px]:justify-evenly dark:bg-black navBartopborder">
+    <nav className="px-3 pt-2 pb-5 bg-white flex flex-col  box-border gap-y-2 w-[244px] h-screen border-r-[1px] border-solid border-[var(--border)]  z-[5] max-[1264px]:w-[72.8px] max-[770px]:fixed max-[770px]:w-full max-[770px]:flex-row max-[770px]:bottom-0 max-[770px]:h-fit max-[770px]:p-0 max-[770px]:justify-evenly dark:bg-black navBartopborder">
       <div className="mt-2 px-3 pt-[25px] mb-[19px] box-border max-[770px]:hidden rounded-lg min-[770px]:max-[1264px]:p-3 hover:min-[770px]:max-[1264px]:bg-gray-200 hover:dark:min-[770px]:max-[1264px]:bg-zinc-800 cursor-pointer logoHoveNav">
         <img
           src="/instagramlogotext.png"
@@ -53,10 +54,12 @@ const Nav = (props: Props) => {
         <AddBoxOutlinedIcon className="w-[24px] h-[24px]"/>
         <div className="pl-4 text-xs max-[1264px]:hidden">Create</div>
       </div>
+      <Link href="/profile">
       <div className="p-3  flex items-center  rounded-lg hover:min-[770px]:bg-gray-200  cursor-pointer  dark:hover:min-[770px]:bg-zinc-800 logoHoveNav">
         <img src="/profile.jpg" alt="" className=" rounded-full" width="24px" />
         <div className="pl-4 text-xs max-[1264px]:hidden ">Profile</div>
       </div>
+      </Link>
       <div className="p-3  flex items-center mt-auto rounded-lg max-[770px]:hidden hover:min-[770px]:bg-gray-200  cursor-pointer  dark:hover:min-[770px]:bg-zinc-800 logoHoveNav">
         <MenuOutlinedIcon className="w-[24px] h-[24px]"/>
         <div className="pl-4 text-xs max-[1264px]:hidden">More</div>
